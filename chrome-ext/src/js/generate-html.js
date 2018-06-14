a536b844159e7dceb1b783983ff9e2d8ec15e079
pastelmind <keepyourhonor@gmail.com>
2018-06-15 06:44:17 +0900

    Added generate-html.js and associated testing code

---
+++ chrome-ext/src/js/generate-html.js
@@ -0,0 +1,69 @@
+/**
+ * Generates HTML from hero/skill/talent data to inject in articles.
+ */
+
+'use strict';
+
+const HtmlGenerator = {
+  /** Placeholder for missing icons */
+  missingIconUrl: 'http://i3.ruliweb.com/img/18/06/15/164006c1bf719dc2c.png',
+
+  /**
+   * Generates a injectable HTML string using the template string and hero data.
+   * @param {string} templateHtml Base template string to work with
+   * @param {Object} hero Hero data object
+   * @param {string} hotsVersion Version string of Heroes of the Storm
+   */
+  generateHeroHtml(templateHtml, hero, hotsVersion) {
+    templateHtml = this._injectHotsVersion(templateHtml, hotsVersion);
+
+    return templateHtml;
+  },
+
+  /**
+   * Generates a injectable HTML string using the template string and skill data.
+   * @param {string} templateHtml Base template string to work with
+   * @param {Object} skill Skill data object
+   * @param {string} hotsVersion Version string of Heroes of the Storm
+   */
+  generateSkillHtml(templateHtml, skill, hotsVersion) {
+    templateHtml = this._injectHotsVersion(templateHtml, hotsVersion);
+
+    return templateHtml.replace(/\{\{name\}\}/g, skill.name)
+      .replace(/\{\{type\}\}/g, skill.type)
+      .replace(/\{\{icon\}\}/g, skill.iconUrl || this.missingIconUrl)
+      .replace(/\{\{icon_alt\}\}/g, skill.name + ' (' + skill.type + ')')
+      .replace(/\{\{description\}\}/g, skill.description);
+  },
+
+  /**
+   * Generates a injectable HTML string using the template string and talent data.
+   * @param {string} templateHtml Base template string to work with
+   * @param {Object} talent Talent data object
+   * @param {string} hotsVersion Version string of Heroes of the Storm
+   */
+  generateTalentHtml(templateHtml, talent, hotsVersion) {
+    templateHtml = this._injectHotsVersion(templateHtml, hotsVersion);
+
+    return templateHtml.replace(/\{\{name\}\}/g, talent.name)
+      .replace(/\{\{type\}\}/g, talent.type)
+      .replace(/\{\{level\}\}/g, talent.level)
+      .replace(/\{\{icon\}\}/g, talent.iconUrl || this.missingIconUrl)
+      .replace(/\{\{icon_alt\}\}/g, talent.name + ' (' + talent.type + ')')
+      .replace(/\{\{description\}\}/g, talent.description);
+  },
+
+  /**
+   * Injects HotS version string into the given template string and returns the
+   * modified template string.
+   * @private
+   * @param {string} html Base template string to work with
+   * @param {string} hotsVersion Version string of Heroes of the Storm
+   */
+  _injectHotsVersion(html, hotsVersion) {
+    if (hotsVersion)
+      return html.replace(/\{\{hots_version\}\}/g, hotsVersion);
+    else  //Remove HotS version row
+      return html.replace(/<tr[^{]*\{\{hots_version\}\}.*?<\/tr>/gi, '');
+  }
+};
