dd445a11113ea8ab1f3022bb94422507708ece90
pastelmind <keepyourhonor@gmail.com>
2018-07-02 10:19:10 +0900

    Rewrote NamuWiki crawl script
    * NamuWiki crawl script can download all skill/talent images (so that they can be uploaded to a 3rd-party site)

--- util/crawl-namu.js
+++ util/crawl-namu.js
@@ -2,13 +2,100 @@
 
 'use strict';
 
-const articleNames = [
+const fs = require('fs');
+const path = require('path');
+const crypto = require('crypto');
+const assert = require('assert');
+const axios = require('axios');
+const entities = new require('html-entities').AllHtmlEntities;
+const namu2hots = require('./src/namu2hots');
+const { Hero } = require('./src/models');
+
+
+/**
+ * Abstract base class that can take an image name and do something with it.
+ */
+class ImageNameConverter {
+  constructor() { this.urls = {}; }
+
+  /**
+   * Add additional name-URL pairs to be tracked by the converter.
+   * @param {Object.<string,string>} urls Mapping of image name => image URL
+   */
+  addUrls(urls) { Object.assign(this.urls, urls); }
+
+  /**
+   * Convert the image name to some other value.
+   * @abstract
+   * @param {string} name Image name
+   * @return {Promise<string>} Value that will be assigned to the image URL property
+   */
+  async convert(name) { throw new Error('Not implemented'); }
+}
+
+/**
+ * Class that downloads the image and computes its hash.
+ */
+class CachedImageHasher extends ImageNameConverter {
+  constructor() {
+    super();
+    this.hashes = {};
+  }
+
+  /**
+   * Downloads the image matching the given name and returns its sha1 hash.
+   * @param {string} name Image name
+   * @return {Promise<string>} sha1 hash string
+   */
+  async convert(name) {
+    if (!(name in this.hashes)) {
+      assert(name in this.urls, name + ' is not associated with any known image URL');
+      const arrBuffer = await downloadArrayBuffer(this.urls[name]);
+      this.hashes[name] = computeHash(arrBuffer);
+    }
+
+    return this.hashes[name];
+  }
+}
+
+/**
+ * Class that downloads and saves the image.
+ */
+class CachedImageDownloader extends ImageNameConverter {
+  /**
+   * @param {string} downloadDir Directory to save the images in.
+   */
+  constructor(downloadDir) {
+    super();
+    this.downloadDir = path.resolve(downloadDir);
+    this.downloaded = {};
+  }
+
+  /**
+   * Downloads and saves the image that matches the given name.
+   * @param {string} name Image name
+   */
+  async convert(name) {
+    if (name in this.downloaded) return;
+
+    assert(name in this.urls, name + ' is not associated with any known image URL');
+    const arrBuffer = await downloadArrayBuffer(this.urls[name]);
+
+    const savePath = path.join(this.downloadDir, name.replace('파일:', ''));
+    fs.writeFileSync(savePath, Buffer.from(arrBuffer));
+
+    this.downloaded[name] = true;
+  }
+}
+
+
+const articleNames = new Set([
   'D.Va(히어로즈 오브 더 스톰)',
   '가로쉬(히어로즈 오브 더 스톰)',
   '가즈로(히어로즈 오브 더 스톰)',
   '겐지(히어로즈 오브 더 스톰)',
   '굴단(히어로즈 오브 더 스톰)',
-  '그레이메인(히어로즈 오브 더 스톰)',
+  '그레이메인',
   '길 잃은 바이킹(히어로즈 오브 더 스톰)',
   '나지보',
   '노바(히어로즈 오브 더 스톰)',
@@ -33,7 +120,6 @@ const articleNames = [
   '메디브(히어로즈 오브 더 스톰)',
   '모랄레스 중위',
   '무라딘(히어로즈 오브 더 스톰)',
-  '바리안(히어로즈 오브 더 스톰)',
   '바리안(히어로즈 오브 더 스톰)',
   '발라(히어로즈 오브 더 스톰)',
   '발리라(히어로즈 오브 더 스톰)',
@@ -67,7 +153,8 @@ const articleNames = [
   '줄진(히어로즈 오브 더 스톰)',
   '첸(히어로즈 오브 더 스톰)',
   '초갈(히어로즈 오브 더 스톰)',
-  '초갈(히어로즈 오브 더 스톰)',
+  '카라짐',
+  '카시아',
   '캘타스(히어로즈 오브 더 스톰)',
   '케리건(히어로즈 오브 더 스톰)',
   '켈투자드(히어로즈 오브 더 스톰)',
@@ -82,76 +169,156 @@ const articleNames = [
   '피닉스(히어로즈 오브 더 스톰)',
   '한조(히어로즈 오브 더 스톰)',
   '해머 상사'
-];
+].sort());
 
-const fs = require('fs');
-const path = require('path');
-const wget = require('wget-improved');
+if (articleNames.size !== 79)
+  console.warn('articleNames.size === %d, expected %d', articleNames.size, 79);
+
 
 const tempDir = path.normalize(process.argv[2] || 'temp/namu-dump');
-const rawDir = path.join(tempDir, 'raw');
-const htmlDir = path.join(tempDir, 'html');
 
-try {
-  prepDir(tempDir);
-  prepDir(rawDir);
-  prepDir(htmlDir);
+(async function () {
+  const imageDownloader = new CachedImageDownloader('temp/namu-images/');
 
-  console.log('Will download articles to', tempDir);
-
-  downloadArticles(articleNames, 'https://namu.wiki/raw/', rawDir, 'txt');
-  downloadArticles(articleNames, 'https://namu.wiki/w/', htmlDir, 'html');
-}
-catch (e) {
-  console.error(e);
-}
-
-
-function prepDir(dirPath) {
-  try {
-    fs.mkdirSync(dirPath);
-    console.log('Created dir:', dirPath);
-  }
-  catch (e) {
+  for (const articleName of articleNames) {
     try {
-      const dirStats = fs.statSync(dirPath);
-      if (!dirStats.isDirectory())
-        throw new Error(dirPath + ' exists, but is not a directory.');
-    }
-    catch (f) {
-      throw f;
+      const hero = await crawlHeroData(articleName);
+      const namuImageUrls = await crawlImageUrls(articleName);
+
+      imageDownloader.addUrls(namuImageUrls);
+
+      if (hero instanceof Hero) //Normal hero
+        forEachIconAsync(hero, imageDownloader);
+      else {  //Cho'Gall
+        forEachIconAsync(hero.cho, imageDownloader);
+        forEachIconAsync(hero.gall, imageDownloader);
+      }
+    } catch (e) {
+      console.error(e);
     }
   }
+})();
+
+
+/**
+ * Downloads the raw article and parses hero data.
+ * @param {string} articleName
+ * @return {Promise<any>} Hero object for most heroes, `{ cho, gall }` for Cho'Gall
+ */
+async function crawlHeroData(articleName) {
+  //Avg 3-second delay with 20% variance
+  const sleepDuration = Math.floor(3000 * (.8 + .4 * Math.random()));
+  process.stdout.write(`Will download ${articleName} after ${sleepDuration} milliseconds...`);
+  await delay(sleepDuration);
+  console.log('commencing');
+
+  const rawArticleResponse = await axios.get(
+    'https://namu.wiki/raw/' + encodeURIComponent(articleName),
+    { responseType: 'text' }
+  );
+
+  const namuMarkup = rawArticleResponse.data;
+
+  //Exception: Cho'Gall is two heroes in one
+  if (articleName.includes('초갈'))
+    return namu2hots.parseChoGallPage(namuMarkup);
+  else
+    return namu2hots.parseHeroPage(namuMarkup);
 }
 
-function downloadArticles(articleNames, urlPrefix, targetDir, fileExt, index = 0) {
-  const articleName = articleNames[index];
-  if (!articleName) return;
 
-  const articleUrl = urlPrefix + encodeURIComponent(articleName);
-  const filePath = path.join(targetDir, articleName.replace('(히어로즈 오브 더 스톰)', '') + '.' + fileExt);
-  
-  const delay = Math.floor(Math.random() * 10000); //To avoid triggering HTTP 429 Too Many Requests
+/**
+ * Downloads the HTML article and parses NamuWiki images.
+ * @param {string} articleName
+ * @return {Promise<Object.<string,string>>} Key-value mapping of NamuWiki image name => image URL.
+ */
+async function crawlImageUrls(articleName) {
+  const htmlArticleResponse = await axios.get(
+    'https://namu.wiki/w/' + encodeURIComponent(articleName),
+    { responseType: 'text' }
+  );
 
-  setTimeout(() => {
-    // //Create file in place to prevent some weird errors
-    // fs.createWriteStream(filePath);
+  return extractImgAltTextToUrl(htmlArticleResponse.data);
+}
 
-    const download = wget.download(articleUrl, filePath);
+/**
+ * Creates a promise that resolves after a given delay.
+ * @param {number} milliseconds Time to wait
+ * @param {*} [value] (Optional) value to resolve with
+ */
+async function delay(milliseconds, value) {
+  return new Promise(resolve => { setTimeout(resolve, milliseconds, value); });
+}
 
-    download.on('error', err => {
-      console.error('Failed to retrieve', articleUrl, err);
-      downloadArticles(articleNames, urlPrefix, targetDir, fileExt, index + 1);
-    });
-    download.on('start', (fileSize) => {
-      console.log('Begin downloading', articleUrl, 'to', filePath, '(content-length: ' + fileSize + ')');
-    });
-    download.on('end', (result) => {
-      console.log('Successfully downloaded', articleUrl, result);
-      downloadArticles(articleNames, urlPrefix, targetDir, fileExt, index + 1);
-    });
+/**
+ * Extracts src and alt attributes from <img> tags in the given HTML.
+ * Will decode HTML entities in both src and alt text
+ * @param {string} html HTML document source
+ * @return {Object.<string, string>} A mapping of alt text: src, or null if there is no <img> tag
+ */
+function extractImgAltTextToUrl(html) {
+  const imgTags = html.match(/<img.*?>/g);
+  if (!imgTags)
+    return null;
 
-  }, delay);
+  const imgAltTextToUrlMapping = {};
+  imgTags.forEach(imgTag => {
+    const srcMatch = /src=['"](.*?)['"]/.exec(imgTag);
+    const altMatch = /alt=['"](.*?)['"]/.exec(imgTag);
+    if (!srcMatch || !altMatch)
+      return;
 
-  console.log('Set delay to', delay);
+    const src = entities.decode(srcMatch[1]);
+    const altText = entities.decode(altMatch[1]);
+    imgAltTextToUrlMapping[altText] = 'https:' + src;
+  });
+
+  return imgAltTextToUrlMapping;
+}
+
+/**
+ * Downloads the URL and returns its contents.
+ * @param {string} url
+ * @return {Promise<ArrayBuffer>}
+ */
+async function downloadArrayBuffer(url) {
+  const response = await axios.get(url, { responseType: 'arraybuffer' });
+  return response.data;
+}
+
+/**
+ * Computes the sha1 hash of the given buffer.
+ * @param {ArrayBuffer} arrayBuffer
+ * @return {string} sha1 hash string
+ */
+function computeHash(arrayBuffer) {
+  const hash = crypto.createHash('sha1');
+  hash.write(Buffer.from(arrayBuffer));
+  return hash.digest('hex');
+}
+
+/**
+ * Applies `converter.convert()` to the iconUrl of every skill/talent of the
+ * given hero object.
+ * @param {Hero} hero
+ * @param {ImageNameConverter} converter
+ */
+async function forEachIconAsync(hero, converter) {
+  for (const skill of hero.skills) {
+    try {
+      skill.iconUrl = await converter.convert(skill.iconUrl);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  for (const talentLevel in hero.talents) {
+    for (const talent of hero.talents[talentLevel]) {
+      try {
+        talent.iconUrl = await converter.convert(talent.iconUrl);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  }
 }
