0e4ba7196d266413f0bd5c5317f817b4ffa2c473
pastelmind <keepyourhonor@gmail.com>
2018-07-08 14:41:14 +0900

    Use $npm_package_version to update manifest.json

--- util/update-manifest.js
+++ util/update-manifest.js
@@ -7,17 +7,21 @@
 if (require.main === module) {
   const fs = require('fs');
 
-  const manifestJsonPath = './chrome-ext/src/manifest.json'
-
-  const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
-  const manifestJson = JSON.parse(fs.readFileSync(manifestJsonPath, 'utf8'));
-
-  if (manifestJson.version === packageJson.version)
-    console.log('manifest.json version is up-to-date (%s), no changes', manifestJson.version);
+  const packageVersion = process.env.npm_package_version;
+  if (!packageVersion) {
+    console.error('Package version is', packageVersion);
+  }
   else {
-    const oldVersion = manifestJson.version;
-    manifestJson.version = packageJson.version;
-    fs.writeFileSync(manifestJsonPath, JSON.stringify(manifestJson, null, 2));
-    console.log('Updated manifest version: %s => %s', oldVersion, manifestJson.version);
+    const manifestJsonPath = './chrome-ext/src/manifest.json';
+    const manifestJson = JSON.parse(fs.readFileSync(manifestJsonPath, 'utf8'));
+
+    if (manifestJson.version === packageVersion)
+      console.log('manifest.json version is up-to-date (%s), no changes', manifestJson.version);
+    else {
+      const oldVersion = manifestJson.version;
+      manifestJson.version = packageVersion;
+      fs.writeFileSync(manifestJsonPath, JSON.stringify(manifestJson, null, 2));
+      console.log('Updated manifest version: %s => %s', oldVersion, manifestJson.version);
+    }
   }
 }
