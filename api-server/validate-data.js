65a4c2ef3f54482f395305f64d3ee75cdcd73e02
pastelmind <keepyourhonor@gmail.com>
2018-08-23 14:38:38 +0900

    Validate hots.json in test data as well

--- api-server/validate-data.js
+++ api-server/validate-data.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+'use strict';
 
 /**
  * Validates hots.json using the schema (hots-schema.json) plus some logical
@@ -11,44 +12,108 @@ const path = require('path');
 const Ajv = require('ajv');
 
 
-//-------- Main code --------//
+/**
+ * Class that validates `hots.json` files.
+ */
+class HotsJsonValidator {
+  /**
+   * Initilize HotsJsonValidator and validates the given JSON schema.
+   * If the JSON schema is invalid, prints the error text to stderr and throws
+   * the error object.
+   * @param {*} hotsDataSchema JSON schema object for `hots.json`
+   */
+  constructor(hotsDataSchema) {
+    this.ajv = new Ajv({ multipleOfPrecision: 5 });
 
+    try {
+      this.schemaValidator = this.ajv.compile(hotsDataSchema);
+    }
+    catch (error) {
+      console.error(this.ajv.errorsText());
+      throw error;
+    }
+  }
+
+  /**
+   * Validates the contents of a `hots.json` file.
+   * If validation fails, prints errors to stderr before throwing errors.
+   * @param {*} hotsDataJson Contents of `hots.json`
+   */
+  validate(hotsDataJson) {
+    //Validate against the schema
+    if (!this.schemaValidator(hotsDataJson)) {
+      console.error(this.ajv.errorsText(this.schemaValidator.errors));
+      throw this.schemaValidator.errors;
+    }
+
+    //Check if hotsData.heroes and hotsData.ptrHeroes are properly ordered by hero name
+    if (!isHeroCollectionSortedByName(hotsDataJson.heroes))
+      throw new Error(`hotsData.heroes is not sorted`);
+
+    if (!isHeroCollectionSortedByName(hotsDataJson.ptrHeroes || {}))
+      throw new Error(`hotsData.ptrHeroes is not sorted`);
+
+    //Check for duplicate stat presets
+    const statPresetIdsFound = {};
+    for (const preset of hotsDataJson.statPresets) {
+      if (preset.id in statPresetIdsFound)
+        throw new Error(`Duplicate stat preset ID found: ${preset.id}`);
+      statPresetIdsFound[preset.id] = preset;
+    }
+
+    const KNOWN_HERO_COUNT = 81;
+
+    //Warn if # of heroes is different than the expected value
+    const heroCount = Object.keys(hotsDataJson.heroes).length;
+    if (heroCount !== KNOWN_HERO_COUNT)
+      console.warn(`Warning: Hero count is ${heroCount}, expected ${KNOWN_HERO_COUNT}`);
+
+    //Warn if # of PTR heroes is bigger than the expected value
+    if (hotsDataJson.ptrHeroes) {
+      const ptrHeroCount = Object.keys(hotsDataJson.ptrHeroes).length;
+      if (ptrHeroCount > KNOWN_HERO_COUNT)
+        console.warn(`Warning: PTR Hero count is ${ptrHeroCount}, expected at most ${KNOWN_HERO_COUNT}`);
+    }
+  }
+}
+
+
+//-------- Main code --------//
 
 const hotsDataSchemaPath = path.join(__dirname, '../docs/hots-schema.json');
 const hotsDataSchema = JSON.parse(fs.readFileSync(hotsDataSchemaPath, 'utf8'));
 console.log('Loaded schema from', hotsDataSchemaPath);
 
-const hotsDataPath = path.join(__dirname, '../docs/hots.json');
-const hotsDataJson = JSON.parse(fs.readFileSync(hotsDataPath, 'utf8'));
-console.log('Loaded HotS data from', hotsDataPath);
 
-const ajv = new Ajv({ multipleOfPrecision: 5 });
-if (!ajv.validate(hotsDataSchema, hotsDataJson)) {
+let hotsJsonValidator;
+
+try {
+  hotsJsonValidator = new HotsJsonValidator(hotsDataSchema);
+  console.log('Passed schema validation');
+}
+catch (error) {
   console.error('Schema validation failure');
-  console.error(ajv.errorsText());
-  throw ajv.errors;
+  throw error;
 }
 
-console.log('Passed schema validation');
+const HOTS_JSON_PATHS = Object.freeze([
+  path.join(__dirname, '../docs/hots.json'),
+  path.join(__dirname, '../chrome-ext/tests/data/hots.json')
+]);
 
+for (const hotsDataPath of HOTS_JSON_PATHS) {
+  const hotsDataJson = JSON.parse(fs.readFileSync(hotsDataPath, 'utf8'));
+  console.log('Loaded HotS data from', hotsDataPath);
 
-//Check if hotsData.heroes and hotsData.ptrHeroes are properly ordered by hero name
-if (!isHeroCollectionSortedByName(hotsDataJson.heroes))
-  throw new Error(`hotsData.heroes is not sorted`);
-
-if (!isHeroCollectionSortedByName(hotsDataJson.ptrHeroes || {}))
-  throw new Error(`hotsData.ptrHeroes is not sorted`);
-
-
-//Check for duplicate stat presets
-const statPresets = {};
-for (const preset of hotsDataJson.statPresets) {
-  if (preset.id in statPresets)
-    throw new Error(`Duplicate stat preset ID found: ${preset.id}`);
-  statPresets[preset.id] = preset;
+  try {
+    hotsJsonValidator.validate(hotsDataJson);
+    console.log('Passed data validation');
+  }
+  catch (error) {
+    console.error('Failed validation for', hotsDataPath);
+    throw error;
+  }
 }
-
-console.log('Passed data validation');
 
 
 //-------- Support functions --------//
