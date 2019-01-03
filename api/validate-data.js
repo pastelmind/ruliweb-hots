b2106e538020b0294c466e5578f1be83ee905a16
pastelmind <keepyourhonor@gmail.com>
2019-01-03 13:31:51 +0900

    Apply HotS PTR patch 2.42.0.71134

--- api/validate-data.js
+++ api/validate-data.js
@@ -55,7 +55,7 @@ class HotsJsonValidator {
     if (!isObjectSortedByKey(hotsDataJson.ptrHeroes || {}))
       throw new Error(`hotsData.ptrHeroes is not sorted`);
 
-    const KNOWN_HERO_COUNT = 84;
+    const KNOWN_HERO_COUNT = 85;
 
     //Warn if # of heroes is different than the expected value
     const heroCount = Object.keys(hotsDataJson.heroes).length;
