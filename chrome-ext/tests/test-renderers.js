f66a09fbf9f17fbeb19d051882a45e94a557f7a0
pastelmind <keepyourhonor@gmail.com>
2019-09-11 00:09:53 +0900

    Manually instantiate Renderer in mocha test

--- chrome-ext/tests/test-renderers.js
+++ chrome-ext/tests/test-renderers.js
@@ -13,43 +13,61 @@ const snapshot = require('snap-shot-it');
 
 const decorateHotsData = require('../src/js/decorate-hots-data');
 
-// Mocks for HotsDialog
-require('./js/mocks');
-const HotsDialog = require('../src/js/hots-dialog');
+const { loadTemplates } = require('./js/mocks');
+const Renderer = require('../src/js/hots-dialog-renderers');
 
 
 describe('HotsDialog.renderers', () => {
   let hotsData;
+  let renderer;
+  const heroFilters = {
+    universe: {
+      name: '세계관',
+      filters: {
+        'warcraft': '워크래프트',
+        'starcraft': '스타크래프트',
+        'diablo': '디아블로',
+        'classic': '블리자드 고전',
+        'overwatch': '오버워치',
+      },
+    },
+    newRole: {
+      name: '역할',
+      filters: {
+        tank: '전사',
+        bruiser: '투사',
+        ranged_assassin: '원거리 암살자',
+        melee_assassin: '근접 암살자',
+        healer: '치유사',
+        support: '지원가',
+      },
+    },
+  };
 
-
-  before('Loading test data files', () => {
+  before('Loading test data files', async () => {
     hotsData = JSON.parse(
       fs.readFileSync(path.join(__dirname, 'data/hots.json'), 'utf8')
     );
     decorateHotsData(hotsData);
+
+    const templates = await loadTemplates();
+    renderer = new Renderer(templates);
   });
 
 
   describe('Dialog templates', () => {
     it('generates dialog content correctly', () => {
-      const dialogHtml = HotsDialog.renderers
-        .renderDialogContent(HotsDialog.heroFilters, hotsData.heroes);
-
-      snapshot(dialogHtml);
+      snapshot(
+        renderer.renderDialogContent(heroFilters, hotsData.heroes)
+      );
     });
 
     it('generates skill icons correctly', () => {
-      const skillIconsHtml =
-        HotsDialog.renderers.renderSkillIcons(hotsData.heroes.Tinker);
-
-      snapshot(skillIconsHtml);
+      snapshot(renderer.renderSkillIcons(hotsData.heroes.Tinker));
     });
 
     it('generates talent list correctly', () => {
-      const talentIconsHtml =
-        HotsDialog.renderers.renderTalentList(hotsData.heroes.Tinker);
-
-      snapshot(talentIconsHtml);
+      snapshot(renderer.renderTalentList(hotsData.heroes.Tinker));
     });
   });
 
@@ -57,30 +75,25 @@ describe('HotsDialog.renderers', () => {
   describe('HotsBox templates', () => {
     it('generates hero boxes correctly', () => {
       const hero = hotsData.heroes.Tinker;
-      const heroBoxFull = HotsDialog.renderers
-        .renderHeroInfoTable(hero, 64, 48, hotsData.hotsVersion);
-      snapshot(heroBoxFull);
-
-      const heroBoxSimple = HotsDialog.renderers
-        .renderHeroInfoTable(hero, 64, 48, hotsData.hotsVersion, true);
-      snapshot(heroBoxSimple);
+      snapshot(
+        renderer.renderHeroInfoTable(hero, 64, 48, hotsData.hotsVersion)
+      );
+      snapshot(
+        renderer.renderHeroInfoTable(hero, 64, 48, hotsData.hotsVersion, true)
+      );
     });
 
     it('generates skill boxes correctly', () => {
       const hero = hotsData.heroes.Fenix;
       for (const skill of hero.skills) {
-        const skillBox = HotsDialog.renderers
-          .renderSkillInfoTable(skill, 64);
-        snapshot(skillBox);
+        snapshot(renderer.renderSkillInfoTable(skill, 64));
       }
     });
 
     it('generates skill boxes (with version) correctly', () => {
       const hero = hotsData.heroes.Fenix;
       for (const skill of hero.skills) {
-        const skillBoxWithVersion = HotsDialog.renderers
-          .renderSkillInfoTable(skill, 64, '34.3');
-        snapshot(skillBoxWithVersion);
+        snapshot(renderer.renderSkillInfoTable(skill, 64, '34.3'));
       }
     });
 
@@ -89,9 +102,7 @@ describe('HotsDialog.renderers', () => {
       for (const talentGroup of Object.values(hero.talents)) {
         // Test only the first talent of each level to save time
         const talent = talentGroup[0];
-        const talentBox = HotsDialog.renderers
-          .renderTalentInfoTable(talent, 48);
-        snapshot(talentBox);
+        snapshot(renderer.renderTalentInfoTable(talent, 48));
       }
     });
 
@@ -100,18 +111,14 @@ describe('HotsDialog.renderers', () => {
       for (const talentGroup of Object.values(hero.talents)) {
         // Test only the first talent of each level to save time
         const talent = talentGroup[0];
-        const talentBoxWithVersion = HotsDialog.renderers
-          .renderTalentInfoTable(talent, 48, '34.3');
-        snapshot(talentBoxWithVersion);
+        snapshot(renderer.renderTalentInfoTable(talent, 48, '34.3'));
       }
     });
 
     it('generates talent box groups correctly', () => {
       const hero = hotsData.heroes.Tinker;
       for (const talentGroup of Object.values(hero.talents)) {
-        const talentBoxGroup = HotsDialog.renderers
-          .renderTalentGroupInfoTable(talentGroup, 48);
-        snapshot(talentBoxGroup);
+        snapshot(renderer.renderTalentGroupInfoTable(talentGroup, 48));
       }
     });
   });
