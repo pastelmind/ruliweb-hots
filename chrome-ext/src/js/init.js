1c2a04692eb6f9f2e2011961f9ee489f4af9618d
pastelmind <keepyourhonor@gmail.com>
2019-09-22 12:38:35 +0900

    Match page URL when adding context menu in Chrome
    For some reason, the WYSIWYG iframe's URL is now identical to the parent
    page URL (Previously, it was always 'about:blank'). Because of this, the
    context menu no longer appears in Chrome.
    
    To fix, change documentUrlPatterns to always match the page URL. Note
    that this was already the case in Firefox.

--- chrome-ext/src/js/init.js
+++ chrome-ext/src/js/init.js
@@ -13,27 +13,18 @@ const ALARM_UPDATE_DATA = 'UPDATE_HOTS_DATA';
 
 // Things that should be called only once (when installed)
 chrome.runtime.onInstalled.addListener(() => {
-  // Run only if Google Chrome
-  if (typeof browser === 'undefined') {
-    chrome.contextMenus.create({
-      'id': 'ruli-context-menu',
-      'title': '히오스 공략툴 열기',
-      'contexts': ['frame'],
-      // Firefox throws an unexpected error on about:blank
-      'documentUrlPatterns': ['about:blank'],
-    });
-  } else { // Run only if Firefox
-    // Firefox does not recognize Ruliweb's WYSIWYG editor as a frame, so create
-    // context menus on all editables instead
-    chrome.contextMenus.create({
-      id: 'ruli-context-menu-test',
-      title: '히오스 공략툴 열기',
-      contexts: ['editable'],
-      // Use the same patterns used for injecting content scripts
-      documentUrlPatterns:
-        chrome.runtime.getManifest().content_scripts[0].matches,
-    });
-  }
+  chrome.contextMenus.create({
+    'id': 'ruli-context-menu',
+    'title': '히오스 공략툴 열기',
+    'contexts': [
+      // Firefox does not recognize Ruliweb's WYSIWYG editor as a frame.
+      // Therefore, create context menus on all editables instead
+      typeof browser === 'undefined' ? 'frame' : 'editable',
+    ],
+    // Use the same patterns used for injecting content scripts
+    'documentUrlPatterns':
+      chrome.runtime.getManifest().content_scripts[0].matches,
+  });
 
   // Load pre-packaged hero data
   updateDataFromUrl(chrome.runtime.getURL('data/hots.json'))
